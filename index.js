3655758a24bc340e79af3a5ad6a0a275209bbcb6
daniel rangel <dr@daniels-MacBook-Pro.local>
2021-07-18 20:19:38 -0400

    version 1.1 removed new keyword  from Router

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 // routes
-app.use('/', require('./src/routes/public-main'));
+app.use('', require('./src/routes/public-main'));
 
 
 
